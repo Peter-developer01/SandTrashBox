d0d488f85bc6f3fa916d76d55e4a089999024643
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2024-04-10 12:11:03 +0300

    game

--- js/1.js
+++ js/1.js
@@ -1,71 +1,73 @@
 console.log("Hello, World!");
 
-const destroyFineOk = document.querySelector("#AAAAAAAAAAAAAA");
-onkeydown = e => {
-	if (e.key === "D") {
-		document.body.style.backgroundColor = "red";
-		destroyFineOk.innerHTML += "D";
+(() => {
+	const destroyFineOk = document.querySelector("#AAAAAAAAAAAAAA");
+	onkeydown = e => {
+		if (e.key === "D") {
+			document.body.style.backgroundColor = "red";
+			destroyFineOk.innerHTML += "D";
 
-		onkeydown = e => {
-			if (e.key === "E") {
-				document.body.style.backgroundColor = "green";
-				destroyFineOk.innerHTML += "es";
+			onkeydown = e => {
+				if (e.key === "E") {
+					document.body.style.backgroundColor = "green";
+					destroyFineOk.innerHTML += "es";
 
-				onkeydown = e => {
-					if (e.key === "S") {
-						document.body.style.filter = "blur(3px)";
-						document.body.style.backgroundColor = "inherit";
-						setTimeout(() => {
-							document.body.style.filter = "none";
-						}, 1000);
-						destroyFineOk.innerHTML += "t";
+					onkeydown = e => {
+						if (e.key === "S") {
+							document.body.style.filter = "blur(3px)";
+							document.body.style.backgroundColor = "inherit";
+							setTimeout(() => {
+								document.body.style.filter = "none";
+							}, 1000);
+							destroyFineOk.innerHTML += "t";
 
-						onkeydown = e => {
-							if (e.key === "T") {
-								document.body.style.transform = "rotate(180deg)";
-								destroyFineOk.innerHTML += "ro";
+							onkeydown = e => {
+								if (e.key === "T") {
+									document.body.style.transform = "rotate(180deg)";
+									destroyFineOk.innerHTML += "ro";
 
-								onkeydown = e => {
-									if (e.key === "R") {
-										document.body.style.transform = "none";
-										destroyFineOk.innerHTML += "y";
+									onkeydown = e => {
+										if (e.key === "R") {
+											document.body.style.transform = "none";
+											destroyFineOk.innerHTML += "y";
 
-										onkeydown = e => {
-											if (e.key === "O") {
-												let past = destroyFineOk.innerHTML;
-												destroyFineOk.innerHTML = "...";
-												setTimeout(() => {
-													destroyFineOk.innerHTML = past;
-													destroyFineOk.innerHTML += "e";
-												}, 1000);
+											onkeydown = e => {
+												if (e.key === "O") {
+													let past = destroyFineOk.innerHTML;
+													destroyFineOk.innerHTML = "...";
+													setTimeout(() => {
+														destroyFineOk.innerHTML = past;
+														destroyFineOk.innerHTML += "e";
+													}, 1000);
 
-												onkeydown = e => {
-													if (e.key === "Y") {
-														destroyFineOk.innerHTML += "d";
-														setTimeout(() => {
-															past = destroyFineOk.innerHTML;
-															destroyFineOk.innerHTML = "PRESS ENTER TO DESTROY";
-															setTimeout(() => destroyFineOk.innerHTML = past, 100);
-														}, 1000);
+													onkeydown = e => {
+														if (e.key === "Y") {
+															destroyFineOk.innerHTML += "d";
+															setTimeout(() => {
+																past = destroyFineOk.innerHTML;
+																destroyFineOk.innerHTML = "PRESS ENTER TO DESTROY";
+																setTimeout(() => destroyFineOk.innerHTML = past, 100);
+															}, 1000);
 
-														onkeydown = e => {
-															if (e.key === "Enter") {
-																onkeydown = null;
-																destroyFineOk.innerHTML += "!";
-																destroyFineOk.style.color = "red";
+															onkeydown = e => {
+																if (e.key === "Enter") {
+																	onkeydown = null;
+																	destroyFineOk.innerHTML += "!";
+																	destroyFineOk.style.color = "red";
 
-																setTimeout(() => {
-																	alert("BOOM!")
-																	document.body.innerHTML = "";
-																	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ%$#@!)(*&^qwertyuiop[]asdfghjkl;'\zxcvbnm";
-																	document.body.style.wordWrap = "anywhere";
-																	const interval = setInterval(() => {
-																		document.body.innerHTML += letters[Math.floor(Math.random() * letters.length)];
-																	}, 100);
 																	setTimeout(() => {
-																		clearInterval(interval);
-																	}, 42424);
-																}, 3000);
+																		alert("BOOM!")
+																		document.body.innerHTML = "";
+																		const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ%$#@!)(*&^qwertyuiop[]asdfghjkl;'\zxcvbnm";
+																		document.body.style.wordWrap = "anywhere";
+																		const interval = setInterval(() => {
+																			document.body.innerHTML += letters[Math.floor(Math.random() * letters.length)];
+																		}, 100);
+																		setTimeout(() => {
+																			clearInterval(interval);
+																		}, 42424);
+																	}, 3000);
+																}
 															}
 														}
 													}
@@ -81,4 +83,25 @@ onkeydown = e => {
 			}
 		}
 	}
-}
+})();
+
+(() => {
+	const btn = document.querySelector("#btu5i");
+	btn.addEventListener("click", () => {
+		alert("BOOM BOOM!");
+	});
+
+	btn.addEventListener("mouseover", () => {
+		setTimeout(() => {
+			btn.setAttribute("disabled", "true");
+			setTimeout(() => btn.removeAttribute("disabled"), 1000);
+		}, 10);
+	});
+})();
+
+(() => {
+	const clickHere = document.querySelector(".btn-click-here");
+	clickHere.addEventListener("click", () => {
+		alert("YOU'VE BEEN RICKROLLED!");
+	})
+})();
---
+++ js/2.js
@@ -0,0 +1,66 @@
+(() => {
+	const letterBox = document.querySelector(".letterbox");
+	const lettersSpan = document.querySelector(".letters");
+	const innerString = lettersSpan.innerText.split("").map(l => `<div class='letter'>${l.replace(" ", "&nbsp;")}</div>`).join("");
+	letterBox.insertAdjacentHTML("beforeend", innerString);
+	lettersSpan.remove();
+
+	const letters = document.querySelectorAll(".letter");
+	letters.forEach(letter => {
+		letter.addEventListener("mouseover", () => {
+			letter.style.transform = "scale(2.0)";
+		});
+		letter.addEventListener("mouseout", () => {
+			letter.style.transform = "scale(1)";
+		});
+
+		// assign random hex color
+		// letter.style.color = `#${Math.floor(Math.random()*16777215).toString(16)}`
+		
+		let start, previousTimeStamp;
+		let done = false;
+		let speed = +`0.00${(Math.floor(Math.random() * 5) + 5) || 1}`
+		console.log(speed)
+
+		window.addEventListener("keydown", (event) => {
+			if (event.key === " " || event.key === letter.innerText) {
+				console.log(event.key)
+				done = false;
+				start = undefined;
+				letter.style.transform = "translateY(0)";
+			}
+		})
+
+		function step(timeStamp) {
+			if (start === undefined) {
+				start = timeStamp;
+			}
+
+			const elapsed = timeStamp - start;
+
+			if (previousTimeStamp !== timeStamp) {
+				// Math.min() is used here to make sure the element stops at exactly 200px
+				const count = Math.min(speed * elapsed, 200);
+				if (count > 165) {
+					letter.remove();
+					if (document.querySelectorAll(".letter").length === 0) {
+						alert("You lose!");
+					}
+					setTimeout(() => new Audio(`audio/${Math.floor(Math.random() * 3) + 1}.mp3`).play(), Math.floor(Math.random() * 200));
+					done = true;
+				}
+				letter.style.transform = `translateY(${count}px)`;
+				if (count === 200) done = true;
+			}
+
+			// if (elapsed < 30000) {
+				previousTimeStamp = timeStamp;
+				if (!done) {
+					window.requestAnimationFrame(step);
+				}
+			// }
+		}
+
+		window.requestAnimationFrame(step);
+	});
+})();
